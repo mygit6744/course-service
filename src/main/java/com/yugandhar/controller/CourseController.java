b01d34bec67eca026e2816a7afce4decda70eb6d
HP <yugamfu@gmail.com>
2024-07-16 14:09:12 +0530

    own test

--- src/main/java/com/yugandhar/controller/CourseController.java
+++ src/main/java/com/yugandhar/controller/CourseController.java
@@ -65,7 +65,7 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-        return "Hello Techie , AWS CICD Example working fine Yugandhar Reddy super success!";
+        return "Hello Techie , AWS CICD Example working fine Yugandhar Reddy super success! Thank you";
     }
 
 
