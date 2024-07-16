814c92a22a588472d4e643e5726308a0f2c64b44
HP <yugamfu@gmail.com>
2024-07-16 10:30:35 +0530

    first commit

--- src/main/java/com/yugandhar/controller/CourseController.java
+++ src/main/java/com/yugandhar/controller/CourseController.java
@@ -65,7 +65,7 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-        return "Hello Techie , AWS CICD Example working fine !";
+        return "Hello Techie , AWS CICD Example working fine Yugandhar !";
     }
 
 
